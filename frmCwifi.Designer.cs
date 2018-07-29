425fe23a3f20e7408b20f27ad642d1ece6bd204e
digi-h <41645479+digi-h@users.noreply.github.com>
2018-07-29 13:03:46 +0100

    Initial Commit

---
+++ frmCwifi.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace cwifi
+{
+    partial class frmCwifi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCwifi));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SSID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Channel = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.MAC_Address = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PHY_Type = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.RSSI = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Signal_Quality = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.AllowColumnReorder = true;
+            this.listView1.AutoArrange = false;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.SSID,
+            this.Channel,
+            this.MAC_Address,
+            this.PHY_Type,
+            this.RSSI,
+            this.Signal_Quality});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.LabelWrap = false;
+            this.listView1.Location = new System.Drawing.Point(12, 91);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(565, 288);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // SSID
+            // 
+            this.SSID.Text = "SSID";
+            this.SSID.Width = 120;
+            // 
+            // Channel
+            // 
+            this.Channel.Text = "Channel";
+            this.Channel.Width = 90;
+            // 
+            // MAC_Address
+            // 
+            this.MAC_Address.Text = "MAC_Address";
+            this.MAC_Address.Width = 120;
+            // 
+            // PHY_Type
+            // 
+            this.PHY_Type.Text = "PHY_Type";
+            this.PHY_Type.Width = 80;
+            // 
+            // RSSI
+            // 
+            this.RSSI.Text = "RSSI";
+            // 
+            // Signal_Quality
+            // 
+            this.Signal_Quality.Text = "Signal_Quality";
+            this.Signal_Quality.Width = 90;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(146, 472);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 52);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(565, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 395);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(590, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.optionsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(590, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "&View";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "&Options";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.toolStrip1.Size = new System.Drawing.Size(590, 25);
+            this.toolStrip1.TabIndex = 7;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmCwifi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 417);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "frmCwifi";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "cwifi - Aailability Scanner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader SSID;
+        private System.Windows.Forms.ColumnHeader Channel;
+        private System.Windows.Forms.ColumnHeader MAC_Address;
+        private System.Windows.Forms.ColumnHeader PHY_Type;
+        private System.Windows.Forms.ColumnHeader RSSI;
+        private System.Windows.Forms.ColumnHeader Signal_Quality;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ frmCwifi.cs
@@ -0,0 +1,196 @@
+﻿/* 
+ * C# managed version of Native Wi-Fi API present on Windows.
+ * http://managedwifi.codeplex.com/ .
+ *
+ * Licence: MIT License(MIT) Copyright(c) 2013 Ilya Konstantinov
+ * 
+ */
+
+using NativeWifi;
+using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Windows.Forms;
+
+namespace cwifi
+{
+
+    public partial class frmCwifi : Form
+    {
+        /// <summary>
+        /// Converts a 802.11 SSID to a string.
+        /// </summary>
+        static string GetStringForSSID(Wlan.Dot11Ssid ssid)
+        {
+            return Encoding.ASCII.GetString(ssid.SSID, 0, (int)ssid.SSIDLength);
+        }
+
+        public frmCwifi()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            RefreshNetworks();
+        }
+
+        private void RefreshNetworks()
+        {
+            comboBox1.Items.Clear();
+            listView1.Items.Clear();
+
+            using (WlanClient client = new WlanClient())
+            {
+                foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
+                {
+                    comboBox1.Items.Add(wlanIface.InterfaceName + " (" + wlanIface.InterfaceGuid + ")");
+
+                    Wlan.WlanAvailableNetwork[] networks = wlanIface.GetAvailableNetworkList(Wlan.WlanGetAvailableNetworkFlags.IncludeAllAdhocProfiles);
+                    Wlan.WlanBssEntry[] bssLists = wlanIface.GetNetworkBssList();
+
+                    foreach (Wlan.WlanAvailableNetwork n in networks)
+                    {
+                        string ssid = GetStringForSSID(n.dot11Ssid);
+                        int rssi = -1;
+                        int channelMHz = -1;
+                        int channel = -1;
+                        string mac_address = "?";
+
+                        // Isolation of the network is not detected via this method?
+                        //Wlan.WlanBssEntry[] bssLists = wlanIface.GetNetworkBssList(network.dot11Ssid, Wlan.Dot11BssType.Any, network.securityEnabled);
+
+                        foreach (var b in bssLists)
+                        {
+                            if (b.dot11Ssid.SSID[0] == n.dot11Ssid.SSID[0])
+                            {
+                                rssi = b.rssi;
+                                channelMHz = (int)b.chCenterFrequency / 1000; // in KHz --> converted to MHz
+
+                                // format the channel frequency - https://en.wikipedia.org/wiki/List_of_WLAN_channels
+
+                                if (channelMHz > 0)
+                                {
+                                    switch (channelMHz) // These are only the 2.4 GHz Channes TODO: Add 5 GHz Channels
+                                    {
+                                        case 2412: // b/g/n
+                                            channel = 1;
+                                            break;
+                                        case 2417: // non-standard
+                                            channel = 2;
+                                            break;
+                                        case 2422: // n
+                                            channel = 3;
+                                            break;
+                                        case 2427: // non-standard
+                                            channel = 4;
+                                            break;
+                                        case 2432: // g/n
+                                            channel = 5;
+                                            break;
+                                        case 2437: // b
+                                            channel = 6;
+                                            break;
+                                        case 2442: // non-standard
+                                            channel = 7;
+                                            break;
+                                        case 2447: // non-standard
+                                            channel = 8;
+                                            break;
+                                        case 2452: // g/n
+                                            channel = 9;
+                                            break;
+                                        case 2457: // non-standard
+                                            channel = 10;
+                                            break;
+                                        case 2462: // b/n
+                                            channel = 11;
+                                            break;
+                                        case 2467: // non-standard
+                                            channel = 12;
+                                            break;
+                                        case 2472: // g/n
+                                            channel = 13;
+                                            break;
+                                        case 2484: // b
+                                            channel = 14;
+                                            break;
+                                    }
+
+                                }
+
+                                PhysicalAddress pa = new PhysicalAddress(n.dot11Ssid.SSID);
+                                mac_address = pa.ToString();
+
+                                // format the mac address:
+                                for (int i = 0; i < mac_address.Length - 1; i++)
+                                {
+                                    if (i % 3 == 0)
+                                    {
+                                        mac_address = mac_address.Insert(i, "-");
+                                    }
+                                }
+
+                                mac_address = mac_address.Remove(0, 1);
+                            }
+                        }
+
+                        List<char> phy_types_supported = new List<char>();
+
+                        foreach (var p in n.Dot11PhyTypes)
+                        {
+                            switch ((int)p)
+                            {
+                                case 4: // Wlan.Dot11PhyType.OFDM:
+                                    phy_types_supported.Add('a'); // 4
+                                    break;
+                                case 6: // Wlan.Dot11PhyType.ERP:
+                                    phy_types_supported.Add('g'); // 6
+                                    break;
+                                case 7:
+                                    phy_types_supported.Add('n'); // 7
+                                    break;
+                            }
+                        }
+
+                        string phy = "?";
+
+                        if (phy_types_supported.Count > 0)
+                        {
+                            phy = "802.11";
+
+                            foreach (var p in phy_types_supported)
+                            {
+                                phy += p;
+                            }
+                        }
+
+                        // https://www.howtogeek.com/197268/how-to-find-the-best-wi-fi-channel-for-your-router-on-any-operating-system/
+                        string[] itemArray = { ssid, channel + " (" + channelMHz + " MHz)", mac_address, phy, rssi.ToString(), n.wlanSignalQuality.ToString() };
+                        listView1.Items.Add(new ListViewItem(itemArray));
+
+                        /*
+                        if (network.dot11DefaultCipherAlgorithm == Wlan.Dot11CipherAlgorithm.WEP)
+                        {
+                            Console.WriteLine("Found WEP network with SSID {0}.", GetStringForSSID(network.dot11Ssid));
+                        }*/
+                    }
+
+                    toolStripStatusLabel1.Text = string.Format("{0} network(s) detected.", listView1.Items.Count);
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            RefreshNetworks();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
